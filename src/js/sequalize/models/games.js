ea5c5bc1a54c9d6582e8d7a8c319f77dd2604a5d
nicomem14 <info@nicolasacosta.es>
2019-06-21 20:11:42 +0100

    more Sequalize Models

--- src/js/sequalize/models/games.js
+++ src/js/sequalize/models/games.js
@@ -2,9 +2,23 @@ const Model = Sequelize.Model;
 
 class games extends Model { }
 games.init({
-    // component of the model.
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    localteam_id: {
+        type: Sequelize.INTEGER,
+    },
+    visitorteam_id: {
+        type: Sequelize.INTEGER,
+    },
 }, {
         sequelize,
         modelName: 'games'
         // options
     });
+
+
+
+
